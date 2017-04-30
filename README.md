a4e1ad501a011eb0129eb3e85a852496f34cc826
Valentyna Kolesnikova (External) <vkolesnikova@blackberry.com>
2017-04-30 21:36:41 +0300

    init empty proj

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # reacdux_a
-Learn React, Redux, NodeJS, MongoDB stack.
+
+========================================
+
+**Pages**
+
+**REGISTRATION** validation message (if any - e.g. username is required, password is required, passwords don't match, registration failed - for any generic error) [ username ] [ password ] [ repeat password ] [ register btn ]
+
+**LOGIN** validation message (if any - e.g. username is required, password is required, incorrect login or password, login failed - for any generic error) [ username ] [ password ] [ login btn ]
+
+**HOME** Hello, You visit us times.
+
+**CHANGE PASSWORD** validation message (if any - e.g. current password is required, password is required, passwords don't match, incorrect password, change password failed) [ old password ] [ new password ] [ repeat password ] [ change password btn ]
+
+========================================
+
+**Technology stack** React/Redux, NodeJS (with express), MongoDB
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  /*entry: './src/app.js',*/
+  entry: './index'
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: [ 'es2015', 'react' ] }
+      }
+    ]
+  }
+};
+
+/*
+'use strict';
+
+const path = require('path');
+const entryPoint = './index';
+module.exports = {
+  context: path.join(__dirname, '/frontend'),
+
+  watch: true,
+
+  entry: {
+    index: entryPoint
+  },
+
+  output: {
+    filename: '[name].js'
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    preLoaders: [
+      {
+        test:    /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, 'frontend')
+        ]
+      }
+    ],
+    loaders: [
+      {
+        test:    /\.js$/,
+        exclude: /node_modules/,
+        loader:  'babel'
+      }
+    ]
+  },
+
+  // define root folders for dependencies
+  resolve: {
+    root: path.resolve('./frontend')
+  },
+
+  devServer: {
+    host:               'localhost',
+    port:               3000,
+    contentBase:        './frontend',
+    headers:            { 'Access-Control-Allow-Origin': '*' },
+    historyApiFallback: {
+      index: entryPoint + '.html'
+    }
+  }
+};
+*/
